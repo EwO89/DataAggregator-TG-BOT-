4d7fdcb3b47f97fde21c7812ba3b06407f1db202
Andrey Kazannikov <andrey.kazannikov@mail.ru>
2024-06-15 12:59:41 +0300

    fix tests problem: motor 3.11 py dependence and etc

--- src/app.py
+++ src/app.py
@@ -1,3 +1,6 @@
+
+
+
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
--- src/tests/test_handlers.py
+++ src/tests/test_handlers.py
@@ -1,14 +1,18 @@
+import sys
 import pytest
-from unittest.mock import AsyncMock, MagicMock, patch, ANY
+from unittest.mock import AsyncMock, patch, ANY, MagicMock
 from aiogram.types import Message, User, Chat
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
+from datetime import datetime, timezone
+from aiogram import Bot, Dispatcher
+
+motor_asyncio_patch = MagicMock()
+motor_asyncio_patch.platform_info.return_value = 'Python/AsyncIO'
+sys.modules['motor.frameworks.asyncio'] = motor_asyncio_patch
+
 from src.bot.handlers import BotHandlers
 from src.config.base import settings
-from src.db import get_db_collections
-from aiogram import Bot, Dispatcher
-from datetime import datetime, timezone
-
 
 
 @pytest.fixture
@@ -17,46 +21,85 @@ def bot():
 
 
 @pytest.fixture
-def dispatcher(bot):
-    return Dispatcher(storage=MemoryStorage())
+def dispatcher(
+        bot
+):
+    dispatcher = Dispatcher()
+    dispatcher['bot'] = bot
+    dispatcher['storage'] = MemoryStorage()
+    return dispatcher
 
 
 @pytest.fixture
 def db_collections():
-    return get_db_collections()
+    users_collection = MagicMock(
+        name='users'
+    )
+    data_collection = MagicMock(
+        name='data_collection'
+    )
+    return {
+        'users': users_collection,
+        'data_collection': data_collection
+    }
 
 
 @pytest.fixture
-def handlers(bot):
-    return BotHandlers(bot)
+def handlers(
+        bot,
+        db_collections
+):
+    users = db_collections['users']
+    data_collection = db_collections['data_collection']
+    return BotHandlers(bot, users, data_collection)
 
 
 @pytest.mark.asyncio
-async def test_start_handler(handlers):
+async def test_start_handler(
+        handlers,
+        dispatcher
+):
     user_id = 123
     user_name = "TestUser"
-    user = User(id=user_id, is_bot=False, first_name=user_name)
-    chat = Chat(id=user_id, type="private")
-    message = Message(
-        message_id=1,
-        from_user=user,
-        chat=chat,
-        date=datetime.now(),
-        text='/start'
+    user = User(
+        id=user_id,
+        is_bot=False,
+        first_name=user_name
     )
+    chat = Chat(
+        id=user_id,
+        type="private"
+    )
+
+    message = MagicMock(
+        spec=Message
+    )
+    message.message_id = 1
+    message.date = datetime.now(timezone.utc)
+    message.chat = chat
+    message.from_user = user
+    message.text = '/start'
     message.answer = AsyncMock()
 
     state = FSMContext(
-        storage=MemoryStorage(), user=message.from_user, chat=message.chat
+        storage=dispatcher['storage'],
+        key=user_id
     )
 
-    with patch.object(handlers.users, 'insert_one', AsyncMock()) as mock_insert:
-        await handlers.start(message, state)
+    with patch.object(
+            handlers.users,
+            'insert_one',
+            AsyncMock()
+    ) as mock_insert:
+        await handlers.start(
+            message,
+            state
+        )
         mock_insert.assert_called_once_with({
             "_id": user_id,
             "name": user_name,
             "created_at": ANY
         })
-        message.answer.assert_called_once_with(f"Привет, {user_name}!")
-
-
+        message.answer.assert_called_once_with(
+            f"Привет, {user_name}!"
+        )
